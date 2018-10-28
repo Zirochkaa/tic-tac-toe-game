bb2816655cacd7e9e5ce25e270376a7d41baa67d
oleh <lovemybeard@gmail.com>
2018-10-28 14:09:04 +0200

    announce winner

--- src/index.js
+++ src/index.js
@@ -37,7 +37,8 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+    const winner = calculateWinner(this.state.squares)
+    const status = winner ? `Winner: ${winner}` : `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
 
     return (
       <div>
